88843af94e69d64f4dd7d28397e2636d5d61dbbf
Bor Grofelnik <luxvoo.me@gmail.com>
2026-01-23 16:46:10 +0100

    Use timezone settings from the host machine

--- create_container.sh
+++ create_container.sh
@@ -2,4 +2,4 @@
 
 podman build -t drgfeed .
 
-podman run --name drg -d -v "./genres.enabled:/genres.enabled" -v "./platforms.enabled:/platforms.enabled" -v "./RAWG_API_KEY:/RAWG_API_KEY" -v "./recipient.conf:/recipient.conf" -v "./msmtp.conf:/msmtp.conf" drgfeed
+podman run --name drg -d -v "./genres.enabled:/genres.enabled" -v "./platforms.enabled:/platforms.enabled" -v "./RAWG_API_KEY:/RAWG_API_KEY" -v "./recipient.conf:/recipient.conf" -v "./msmtp.conf:/msmtp.conf" -v "/etc/localtime:/etc/localtime:ro" drgfeed
