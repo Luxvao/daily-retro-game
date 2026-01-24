1be5e5df0601e0fb12168c23ae0bc5aee7dfb507
Bor Grofelnik <luxvoo.me@gmail.com>
2026-01-24 22:42:38 +0100

    Finished the random game fetch part

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,39 @@
-use crate::config::Config;
+use color_eyre::eyre::Result;
+use rand::Rng;
+
+use crate::{
+    config::Config,
+    rawg::{get_description, get_page},
+};
 
 #[derive(Clone, Debug)]
 pub struct Game {
     pub name: String,
-    pub release: String,
-    pub image: String,
+    pub id: u32,
+    pub release: Option<String>,
+    pub image: Option<String>,
     pub platforms: Vec<String>,
     pub description: Option<String>,
 }
 
 impl Game {
-    pub fn random(config: &Config) -> Game {
-        todo!()
+    pub fn random(config: &Config) -> Result<Game> {
+        let mut rng = rand::rng();
+
+        let (total_games, _) = get_page(config, 1)?;
+
+        let total_pages = (total_games + 39) / 40 + 1;
+
+        let random_page = rng.random_range(1..total_pages);
+
+        let (_, games) = get_page(config, random_page)?;
+
+        let random_index = rng.random_range(0..games.len());
+
+        let mut game = games[random_index].clone();
+
+        game.description = get_description(config, game.id)?;
+
+        Ok(game)
     }
 }
--- src/rawg.rs
+++ src/rawg.rs
@@ -1,10 +1,10 @@
-use color_eyre::{Result, eyre::ContextCompat};
+use color_eyre::{eyre::ContextCompat, Result};
 
 use crate::{config::Config, game::Game};
 
-pub fn get_page(config: &Config, page_n: u32) -> Result<Vec<Game>> {
+pub fn get_page(config: &Config, page_n: u32) -> Result<(u32, Vec<Game>)> {
     let resp = reqwest::blocking::get(format!(
-        "https://api.rawg.io/api/games?key={}&genres={}&platforms={}&page={}",
+        "https://api.rawg.io/api/games?key={}&genres={}&platforms={}&page={}&page_size=40",
         config.rawg_key,
         config
             .genres
@@ -25,7 +25,7 @@ pub fn get_page(config: &Config, page_n: u32) -> Result<Vec<Game>> {
 
     let mut games = Vec::new();
 
-    for game in json_parsed.members() {
+    for game in json_parsed["results"].members() {
         let platforms = game["platforms"]
             .members()
             .map(|platform| {
@@ -41,14 +41,13 @@ pub fn get_page(config: &Config, page_n: u32) -> Result<Vec<Game>> {
                 .as_str()
                 .context("Game name is not a string")?
                 .to_string(),
+            id: game["id"].as_u32().context("Game ID is not an integer")?,
             release: game["released"]
                 .as_str()
-                .context("Date of release is not a string")?
-                .to_string(),
+                .map(|released| released.to_string()),
             image: game["background_image"]
                 .as_str()
-                .context("Background image URL is not a string")?
-                .to_string(),
+                .map(|image| image.to_string()),
             platforms: platforms,
             description: None,
         };
@@ -56,5 +55,23 @@ pub fn get_page(config: &Config, page_n: u32) -> Result<Vec<Game>> {
         games.push(game_struct);
     }
 
-    Ok(games)
+    Ok((
+        json_parsed["count"]
+            .as_u32()
+            .context("Count of games is not an integer")?,
+        games,
+    ))
+}
+
+pub fn get_description(config: &Config, id: u32) -> Result<Option<String>> {
+    let resp = reqwest::blocking::get(format!(
+        "https://api.rawg.io/api/games/{}?key={}",
+        id, config.rawg_key
+    ))?;
+
+    let json_parsed = json::parse(resp.text()?.as_str())?;
+
+    Ok(json_parsed["description"]
+        .as_str()
+        .map(|desc| desc.to_string()))
 }
